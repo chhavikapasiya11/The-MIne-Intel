aae2a8c635342c08e2230e0f765aa6caf6180781
Gaurangi25 <gaurangi250704@gmail.com>
2025-11-19 03:25:50 +0530

    frontend and backend added

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask,request,jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 model = joblib.load("../models/Mining_CatBoost_Model.joblib")
 preprocess = joblib.load("../models/preprocessing_pipeline_catboost.joblib")
