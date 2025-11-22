5ef72ffae0fdb058f167882bc1e0ad87b2d28f6e
Gaurangi25 <gaurangi250704@gmail.com>
2025-11-23 02:35:45 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 model = joblib.load("../models/Mining_CatBoost_Model.joblib")
-preprocess = joblib.load("../models/preprocessing_pipeline_catboost.joblib")
+preprocess = joblib.load("../models/preprocessing_pipeline_catboost_final.joblib")
 
 @app.route("/")
 def home():
